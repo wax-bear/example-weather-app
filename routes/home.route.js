7d9f564c24fecf5c201780526fa85fe5713deedc
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-10-29 21:24:27 +0100

    :recycle: refactored the post route of home.route.js

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,33 +1,32 @@
 const request = require("request");
 const { Router } = require("express");
-const router = Router();
 const { apiKey, urlOneApiKey, urlOneCityQuery } = require("../configs");
+
+const router = Router();
 
 router.get("/", (_, res) =>
   res.render("index", { weather: null, error: null })
 );
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  let city = req.body.city;
-  let url = `${urlOneApiKey}${city}${urlOneCityQuery}${apiKey}`;
-  request(url, function (err, response, body) {
+  const { city } = req.body;
+  const url = `${urlOneCityQuery}${city}${urlOneApiKey}${apiKey}`;
+
+  request(url, function (err, _, body) {
     if (err) {
       res.render("index", { weather: null, error: "Error, please try again" });
     } else {
-      let weatherResponse = JSON.parse(body);
-      console.log(weatherResponse);
-      if (weatherResponse.main == undefined) {
-        res.render("index", {
-          weather: null,
-          error: "Error, please try again",
-        });
-      } else {
-        let weather = `It's ${Math.floor(
-          weatherResponse.main.temp
-        )} degrees in ${weatherResponse.name}!`;
-        res.render("index", { weather, error: null });
-      }
+      const weatherResponse = JSON.parse(body);
+      const { main, name } = weatherResponse;
+      const { temp } = main;
+      const weather = `It's ${Math.floor(temp)} degrees in ${name}!`;
+
+      main == undefined
+        ? res.render("index", {
+            weather: null,
+            error: "Error, please try again",
+          })
+        : res.render("index", { weather, error: null });
     }
   });
 });
